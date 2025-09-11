e8b86cb5d28840c2a3f15860af1bce04f7177777
onore_gleb <onoreg@mail.ru>
2025-09-11 18:13:22 +0300

    del 4bit quant

--- app/services/medgemma.py
+++ app/services/medgemma.py
@@ -16,16 +16,9 @@ class MedGemmaService:
 
             model_id = "google/medgemma-4b-it"
 
-            quantization_config = BitsAndBytesConfig(
-                load_in_4bit=True,
-                bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch.bfloat16,
-            )
-
             cls._processor = AutoProcessor.from_pretrained(model_id)
             cls._model = LlavaForConditionalGeneration.from_pretrained(
                 model_id,
-                quantization_config=quantization_config,
                 torch_dtype=torch.bfloat16,
                 low_cpu_mem_usage=True,
             )
